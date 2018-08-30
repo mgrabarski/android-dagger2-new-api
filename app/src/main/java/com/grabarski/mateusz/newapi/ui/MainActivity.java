480449210729605e44ea449cc40e249bb6eb8451
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-08-30 19:55:50 +0200

    Call inject in MainActivity

--- app/src/main/java/com/grabarski/mateusz/newapi/ui/MainActivity.java
+++ app/src/main/java/com/grabarski/mateusz/newapi/ui/MainActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 
 import com.grabarski.mateusz.newapi.R;
 
+import dagger.android.AndroidInjection;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
